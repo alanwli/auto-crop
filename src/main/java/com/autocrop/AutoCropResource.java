fc20859c1e0cd1d74017d0e62b4830f383177806
alan <alanwli@gmail.com>
2015-07-11 14:20:21 -0700

    Download pipe established.

--- src/main/java/com/autocrop/AutoCropResource.java
+++ src/main/java/com/autocrop/AutoCropResource.java
@@ -1,21 +1,23 @@
 package com.autocrop;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-
-import java.io.InputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.util.UUID;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 @Path("/auto-crop")
 public class AutoCropResource {
@@ -23,9 +25,18 @@ public class AutoCropResource {
   @GET
   @Path("/fetch")
   //@Produces({"application/octet-stream"})
-  @Produces(MediaType.TEXT_PLAIN)
+  //@Produces(MediaType.TEXT_PLAIN)
+  @Produces({"image/jpeg"})
   public Response streamingFetch() {
-    return Response.ok("foo").build();
+    StreamingOutput streamOutput = new StreamingOutput() {
+      @Override
+      public void write(OutputStream os) throws IOException {
+        // TODO
+        java.nio.file.Path path = FileSystems.getDefault().getPath("/tmp/auto-crop/3b6ceb600f1540e68d2e83c2e1ede55c/0.jpg");
+        Files.copy(path, os);
+      }
+    };
+    return Response.ok(streamOutput).build();
   }
 
   @POST
